f867e7c1279c7e3a46f8c0f01ed762e4e65e7fa1
ashraful-19 <68117550+ashraful-19@users.noreply.github.com>
2023-04-17 22:47:23 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ app.use(passport.session());
 
 
 app.use(flash());
+
 app.use((req,res,next) => {
   if (req.isAuthenticated()) {
     console.log(req.user)
@@ -59,7 +60,8 @@ app.use(function(req, res, next) {
   res.locals.req = req;
   res.locals.isAuthenticated = req.isAuthenticated();
   res.locals.currentUser = req.user;
-  
+  res.locals.flash = req.flash();
+
   next();
 });
 
