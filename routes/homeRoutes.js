eb3e9310c92027e7ab49b45d16356f0d83e4c66e
ashraful-19 <68117550+ashraful-19@users.noreply.github.com>
2023-10-11 03:09:56 +0600

    Update homeRoutes.js

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -4,9 +4,11 @@ const app = express();
 const router = express.Router();
 const { checkAuthenticated, checkLoggedIn } = require('../config/auth');
 const { checkPayment,checkAccess } = require("../middlewares/updateUser");
-
+getTerms&Conditions
 router.get('/', homeController.getIndex);
 router.get('/doubts',checkAuthenticated, homeController.getDoubts);
+
+router.get('/terms&conditions', homeController.getTerms&Conditions);
 router.get('/course', homeController.getCourses);
 router.get('/profile',checkAuthenticated, homeController.getProfile);
 router.get('/course-details/:id', checkPayment ,homeController.getCourseDetails);
